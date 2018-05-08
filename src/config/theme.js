a9f2a6215c6dd47aecbd1afed307c1b7055589df
Manzoor Samad <manzoorsamad.in@gmail.com>
2018-05-08 15:05:53 +0400

    created theme file for app

---
+++ src/config/theme.js
@@ -0,0 +1,11 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+
+EStyleSheet.build({
+  $colorPrimary: '#4527A0',
+  $colorSecondary: '#fff',
+  $colorTertiary: '#ecf0f1',
+  $darkText: '#131418',
+  $lightText: '#fff',
+  $primaryFontColor:'#4527A0',
+});
