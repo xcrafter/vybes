c951fb676b3ec2928689491c646075e8c8a88eb6
Manzoor Samad <manzoorsamad.in@gmail.com>
2018-05-08 19:59:20 +0400

    Added Routes

--- App.js
+++ App.js
@@ -1,58 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { StackNavigator } from 'react-navigation';
+import Home from '@scenes/home';
+import Influencer from '@scenes/influencer';
+import Items from '@scenes/items';
+import theme from '@config/theme';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const homeRouter = StackNavigator({
+  home: Home,
+  influencer: Influencer,
+  items: Items,
+}, {
+  initialRouteName: 'items',
+  navigationOptions: {
+    header: null,
+  },
+  lazy: true,
+  animationEnabled: true,
+  tabBarOptions: {
+    style: {
+      backgroundColor: '#fff',
+    },
+  },
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default homeRouter;
