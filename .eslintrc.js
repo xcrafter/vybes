c38aad2c69cd3074ddb4c5f85b6261ec0381c14f
Manzoor Samad <manzoorsamad.in@gmail.com>
2018-05-08 14:47:39 +0400

    Initial Project Setup

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+    },
+    "rules":{
+        "react/prefer-stateless-function":0,
+        "react/no-unused-state":0,
+        "react/jsx-filename-extension":0,
+        "react/forbid-prop-types":0,
+        "import/prefer-default-export":0,
+    }
+};
