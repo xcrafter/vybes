611df86ac2e27a24add2accfa28d741e3649521a
Manzoor Samad <manzoorsamad.in@gmail.com>
2018-05-09 00:49:51 +0400

    added transparent header

--- App.js
+++ App.js
@@ -3,15 +3,24 @@ import Home from '@scenes/home';
 import Influencer from '@scenes/influencer';
 import Items from '@scenes/items';
 import theme from '@config/theme';
+import Loading from '@components/loadingScreen';
 
 const homeRouter = StackNavigator({
   home: Home,
   influencer: Influencer,
   items: Items,
+  loading: Loading,
 }, {
-  initialRouteName: 'items',
+  initialRouteName: 'home',
   navigationOptions: {
-    header: null,
+    headerStyle: {
+      backgroundColor: 'transparent',
+      right: 0,
+      left: 0,
+      top: 0,
+      position: 'absolute',
+      borderBottomWidth: 0,
+    },
   },
   lazy: true,
   animationEnabled: true,
